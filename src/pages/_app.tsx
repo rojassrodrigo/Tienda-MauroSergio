27f3fb9d270c10b83cf5a485568fc22a0f6b672a
rojassrodrigo <rojasrodrigo@live.com>
2023-04-26 20:27:10 -0300

    five commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,27 @@
 // pages/_app.js
 import React from 'react';
-import { Box, ChakraProvider, Container, Divider, Heading, Image, Text, VStack } from '@chakra-ui/react'
+import { Box, ChakraProvider, Container, Divider, Heading, Image, Stack, Text } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
+import logo from '../styles/logoStore2.png';
 import theme from '../../theme';
 
-const App: React.FC<AppProps> =({ Component, pageProps }) => {
+const paddingSize = 5;
+const borderRadiusSize = 'sm';
+const maxWidthSize = 'container.xl';
+
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
-      <Box padding={5}>
-      <Container backgroundColor='white'
-        borderRadius="sm"
-        boxShadow='md'
-        maxWidth='container.xl'
-        padding={4}
-        >
-        <VStack marginBottom={6}>
-          <Image borderRadius={9999} src="//placeholder.it/128x128"></Image>
-          <Heading>Suplementos Cordoba</Heading>
-          <Text>Tienda de Suplementos</Text>
-        </VStack>
-        <Divider marginY={6}/>
-       <Component {...pageProps} />
-      </Container>
+      <Box padding={paddingSize}>
+        <Container backgroundColor='white' borderRadius={borderRadiusSize} boxShadow='md' maxWidth={maxWidthSize} padding={4}>
+          <Stack spacing={4} align='center'>
+            <Image maxW="45%" maxH="35%" src={logo.src} />
+            {/* <Heading>ProteinX</Heading>
+            <Text>Somos una tienda de Suplementos Deportivos con los mejores precios de Cordoba Capital</Text> */}
+          </Stack>
+          <Divider marginY={6} marginBottom={6} />
+          <Component {...pageProps} />
+        </Container>
       </Box>
     </ChakraProvider>
   )
